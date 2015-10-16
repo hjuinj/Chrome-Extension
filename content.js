a496884b54663f04ece4c3b443719f7141e6d1e7
hjuinj <sw4512@ic.ac.uk>
2015-10-16 22:09:33 +0100

    disable when in textfield

--- content.js
+++ content.js
@@ -1,21 +1,31 @@
 // seem to run when a page is loaded:  alert("hello");
 function VimLikeness(keypressed){
-    switch(keypressed.keyCode){
-        case 74:
-            window.scrollBy(0,50);
-            break;
-        case 75:
-            window.scrollBy(0,-50);
-            break;
-        case 72:
-            document.body.scrollLeft -=10;
-            break;
-        case 76:
-            document.body.scrollLeft +=10;
-            break;
-        default:
-            break;
+    var el = document.activeElement;
+    //console.log(el.type == undefined);
+    //if (el && !(el.tagName.toLowerCase() != 'input' && el.type != 'text' || el.tagName.toLowerCase() != 'textarea')) {
+    //
+    //condition to test focus is not in a textfield for entering text
+    if(el && el.type == undefined) {
+        
+        
+        switch(keypressed.keyCode){
+            case 74:
+                window.scrollBy(0,50);
+                break;
+            case 75:
+                window.scrollBy(0,-50);
+                break;
+            case 72:
+                document.body.scrollLeft -=10;
+                break;
+            case 76:
+                document.body.scrollLeft +=10;
+                break;
+            default:
+                break;
+        }
     }
+    
 }
 
 
